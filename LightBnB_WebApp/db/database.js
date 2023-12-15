cef3778a7a5f9e471dc55451cbf6e26811303fb9
M-ARTHELL <makoarthell@proton.me>
2023-12-15 18:59:13 -0400

    refactor getAllReservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -65,7 +65,6 @@ const addUser = function (user) {
   return pool
     .query(queryString, [val1, val2, val3])
     .then((result) => {
-      console.log(user)
       return result.rows[0];
     })
     .catch((err) => {
@@ -82,7 +81,24 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `SELECT properties.*, reservations.id, reservations.start_date, AVG(property_reviews.rating) AS "average_rating"
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.reservation_id = reservations.id
+  WHERE reservations.guest_id = $1
+  GROUP BY reservations.id, properties.title, properties.cost_per_night, reservations.start_date, properties.id
+  ORDER BY reservations.start_date
+  LIMIT $2`
+
+  return pool
+    .query(queryString, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return null;
+    });
 };
 
 /// Properties
