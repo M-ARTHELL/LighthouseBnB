d0e6f088f59e807e0a0d7c44fe9d43d3cf81596b
M-ARTHELL <makoarthell@proton.me>
2023-12-16 22:07:34 -0400

    add comments

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -9,22 +9,24 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-/// Users
 
-/**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+/////////////////////////////////////////////////////
+/// USERS
+/////////////////////////////////////////////////////
 
+// getUserWithEmail
+// gets user with specified email
 const getUserWithEmail = function (email) {
   const queryString = 'SELECT * FROM users WHERE email = $1'
 
+  // returns results as promise
   return pool
     .query(queryString, [email])
     .then((result) => {
       return result.rows[0];
     })
+
+    // if error: show in console
     .catch((err) => {
       console.log(err.message);
       return null;
@@ -32,54 +34,56 @@ const getUserWithEmail = function (email) {
 
 };
 
-/**
- * Get a single user from the database given their id.
- * @param {string} id The id of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+
+// getUserWithId
+// gets user with specified id
 const getUserWithId = function (id) {
   const queryString = 'SELECT * FROM users WHERE id = $1'
 
+  // returns results as promise
   return pool
     .query(queryString, [id])
     .then((result) => {
       return result.rows[0];
     })
+
+    //if error: show in console
     .catch((err) => {
       console.log(err.message);
       return null;
     });
 };
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
+
+// addUser
+// adds a new user to the database
 const addUser = function (user) {
   const queryString = 'INSERT INTO users(name, email, password) VALUES ($1, $2, $3) RETURNING *'
   const val1 = user.name;
   const val2 = user.email;
   const val3 = user.password;
 
+  // return results as promise
   return pool
     .query(queryString, [val1, val2, val3])
     .then((result) => {
       return result.rows[0];
     })
+
+    // if error: show in console
     .catch((err) => {
       console.log(err.message);
       return null;
     });
 };
 
-/// Reservations
 
-/**
- * Get all reservations for a single user.
- * @param {string} guest_id The id of the user.
- * @return {Promise<[{}]>} A promise to the reservations.
- */
+/////////////////////////////////////////////////////
+/// RESERVATIONS
+/////////////////////////////////////////////////////
+
+// getAllReservations
+// gets resevations based on user id.
 const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `SELECT properties.*, reservations.id, reservations.start_date, AVG(property_reviews.rating) AS "average_rating"
   FROM reservations
@@ -90,32 +94,35 @@ const getAllReservations = function (guest_id, limit = 10) {
   ORDER BY reservations.start_date
   LIMIT $2`
 
+  // return results as promise
   return pool
     .query(queryString, [guest_id, limit])
     .then((result) => {
       return result.rows;
     })
+
+    // if error: show in console
     .catch((err) => {
       console.log(err.message);
       return null;
     });
 };
 
-/// Properties
 
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
+/////////////////////////////////////////////////////
+/// PROPERTIES
+/////////////////////////////////////////////////////
 
+// getAllProperties
+// obtains all relevant properties and handles any search options.
 const getAllProperties = function (options, limit = 10) {
   let queryString = `SELECT properties.*, AVG(property_reviews.rating) AS "average_rating"
   FROM properties
   JOIN property_reviews ON properties.id = property_id `
   const queryParams = [];
 
+  // the following checks for search options and adds them to the query as needed.
+  // city
   if (options.city) {
     if (queryParams.length > 0) {
       queryString += ' AND '
@@ -126,6 +133,7 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `city LIKE initcap($${queryParams.length})`;
   }
 
+  // property owner
   if (options.owner_id) {
     if (queryParams.length > 0) {
       queryString += ' AND '
@@ -136,6 +144,7 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `owner_id = $${queryParams.length}`;
   }
 
+  // min price
   if (options.minimum_price_per_night) {
     if (queryParams.length > 0) {
       queryString += ' AND '
@@ -146,6 +155,7 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `cost_per_night >= $${queryParams.length}`
   }
 
+  // max price
   if (options.maximum_price_per_night) {
     if (queryParams.length > 0) {
       queryString += ' AND '
@@ -156,51 +166,61 @@ const getAllProperties = function (options, limit = 10) {
     queryString += `cost_per_night <= $${queryParams.length}`
   }
 
+  // adds 'GROUP BY' before possible 'HAVING' so the query will function correctly.
   queryString += `GROUP BY properties.id `;
 
+  // avg reviews
   if (options.minimum_rating) {
     queryString += 'HAVING '
     queryParams.push(`${options.minimum_rating}`);
     queryString += `AVG(property_reviews.rating) >= $${queryParams.length}`
   }
 
+  // adds 'ORDER BY' and 'LIMIT' to query.
   queryParams.push(limit);
   queryString += `
   ORDER BY cost_per_night
   LIMIT $${queryParams.length};`
 
+  // return results as promise
   return pool
     .query(queryString, queryParams)
     .then((result) => {
       return result.rows
     })
+
+    // if error: show in console
     .catch((err) => {
       console.log(err.message);
     });
 };
 
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
+
+// addProperty
+// adds a new property to the database using form input
 const addProperty = function (property) {
 
   const queryString = `INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`
-  queryParams = [];
+
+  let queryParams = [];
 
   queryParams.push(property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms)
 
+  
+  // return results as promise
   return pool
     .query(queryString, queryParams)
       .then((result) => {
       return result.rows[0];
     })
+
+    // if error: show in console
     .catch((err) => {
       console.log(err.message);
       return null;
     });
 };
+
 
 module.exports = {
   getUserWithEmail,
